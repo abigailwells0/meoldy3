30b2ac1746d396ca6053aede128c75bc2b1d920a
abigailwells0 <45012134+abigailwells0@users.noreply.github.com>
2020-02-06 11:52:35 -0800

    Add files via upload

--- Melody_Samples.rb
+++ Melody_Samples.rb
@@ -1,50 +1,148 @@
-# Stranger Things
-st_notes = [:c3, :e3, :g3, :b3, :c4, :b3, :e3, :c3]
-index = 0
-
-use_synth :chipbass
-live_loop :main do
-  with_fx :distortion do
-    play st_notes[index]
-    index = index + 1
-    sleep 0.25
-    if index == 7
-      index = 0
-    end
-    print index
+# Welcome to Sonic Pi v3.1
+use_bpm 158
+use_synth :piano
+intro ="C:/Users/abigail_wells/Documents/Audacity/BTS speech at the United Nations UNICEF.wav"
+vocals ="C:/Users/abigail_wells/Documents/Audacity/BTS.wav"
+ending = "C:/Users/abigail_wells/Documents/Audacity/i_need_u.wav"
+n = [:C5, :Ab4, :F4]
+n2 = [:G4,:Eb4, :Bb4]
+n3 = [:Ab4,:F4, :Db4]
+a=0
+sample intro, amp: 2
+sleep 50
+live_loop :low do
+  4.times do
+    play n[a], sustain: 3
+    a=a+1
+    play n[a], sustain: 3
+    a=a+1
+    play n[a], sustain: 3
+    sleep 4
+    a=0
+    play n2[a], sustain:  3
+    a=a+1
+    play n2[a], sustain:  3
+    a=a+1
+    play n2[a], sustain:  3
+    sleep 4
+    a=0
+    play n3[a], sustain: 5
+    a=a+1
+    play n3[a], sustain: 5
+    a=a+1
+    play n3[a], sustain: 5
+    sleep 8
+    a=0
   end
+  stop
+end
+sleep 16
+
+live_loop :hi do
+  3.times do
+    sleep 2
+    play :Bb5
+    sleep 0.5
+    play :C6
+    sleep 0.5
+    play :Eb6
+    sleep 0.5
+    play :C6
+    sleep 2.5
+    play :C6
+    sleep 0.5
+    play :Bb5
+    sleep 0.5
+    play :Ab5
+    sleep 0.5
+    play :F5
+    sleep 1.5
+    play :Ab5, sustain: 1.5
+    sleep 1
+    play :Bb5
+    sleep 0.5
+    play :Eb6
+    sleep 1
+    play :C6, sustain: 1.5
+    sleep 1
+    play :C6
+    sleep 0.5
+    play :C6, sustain: 1.5
+    sleep 1
+    play :F6
+    sleep 0.5
+    play :Eb6, sustain: 1.5
+    sleep 1
+    play :C6
+    sleep 0.5
+  end
+  x=1
+  2.times do
+    sleep 2
+    play :Bb5, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :C6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :Eb6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :C6, amp: x
+    x=x-0.02941176
+    sleep 2.5
+    play :C6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :Bb5, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :Ab5, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :F5, amp: x
+    x=x-0.02941176
+    sleep 1.5
+    play :Ab5, sustain: 1.5, amp: x
+    x=x-0.02941176
+    sleep 1
+    play :Bb5, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :Eb6, amp: x
+    x=x-0.02941176
+    sleep 1
+    play :C6, sustain: 1.5, amp: x
+    x=x-0.02941176
+    sleep 1
+    play :C6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :C6, sustain: 1.5, amp: x
+    x=x-0.02941176
+    sleep 1
+    play :F6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+    play :Eb6, sustain: 1.5, amp: x
+    x=x-0.02941176
+    sleep 1
+    play :C6, amp: x
+    x=x-0.02941176
+    sleep 0.5
+  end
+  stop
+end
+sleep 14.25
+live_loop :vocals do
+  sample vocals, release: 0.5
+  sleep 32
+  stop
+end
+sleep 59.02
+live_loop :ending do
+  sample ending, release: 0.5, amp: 0.5
+  sleep 3
+  stop
 end
 
-use_synth :chiplead
-live_loop :back do
-  play :C1
-  play :C2
-  sleep 4
-  play :E1
-  play :E2
-  sleep 4
-  play :C1
-  play :C2
-  sleep 4
-  play :D1
-  play :D2
-  sleep 0.5
-  play :E1
-  play :E2
-  sleep 4
-  play :D1
-  play :D2
-  sleep 0.5
-  play :C1
-  play :C2
-  sleep 4
-  play :D1
-  play :D2
-  sleep 0.5
-  play :C1
-  play :C2
-  sleep 0.5
-  play :B
-  play :B1
-  sleep 4
-end
